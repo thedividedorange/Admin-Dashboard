047d6acf62b78b825f9962aeb78d5233a154508a
thedividedorange <spawnpoint13@gmail.com>
2024-08-27 13:54:52 +0100

    update cards section with projects and styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,29 +8,29 @@ module.exports = {
     extend: {
       colors: {
         // Background colors
-        'background-dark': '#131920', //  nav bg color
-        'background-dark-secondary': '#1b232d', // card
-        'background-dark-hover': 'rgb(35, 35, 35, 0.55)', // hover
+        'background-dark': '#131920',
+        'background-dark-secondary': '#1b232d',
+        'background-dark-hover': 'rgb(35, 35, 35, 0.55)',
         
         // Text colors
         'text-dark-primary': 'rgba(255, 255, 255, 0.8)',
         'text-dark-secondary': 'rgba(255, 255, 255, 0.5)',
-        
-        'text-active-color': '#4680ff',
 
         // Accent colors
-        'accent-dark': '#4680ff', // For buttons, highlights, etc.
+        'accent-dark': '#4680ff',
         'accent-dark-hover': '#3c6dd9',
 
         // Border colors
-        'border-dark': '#2e2e2e', //  nav border color
-        'border-dark-secondary': '#263240', // card border color
-        'border-btn': '#4680ff',
-        'border-btn-hover': '#3866cc',
+        'border-dark': '#2e2e2e',
+        'border-dark-secondary': '#263240',
       },
       dropShadow: {
         'dark-primary': '0 4px 3px rgba(0, 0, 0, 1)',
         'dark-secondary': '0 4px 3px rgb(255 255 255 / 0.22)',
+      },
+      boxShadow: {
+        'card-shadow-dark': '0 10px 15px -3px rgba(37,99,235,0.68)',
+        'card-image-inset': 'inset 0 0px 6px 2px rgb(0 0 0 / 77%)'
       },
       keyframes: {
         spin: {
